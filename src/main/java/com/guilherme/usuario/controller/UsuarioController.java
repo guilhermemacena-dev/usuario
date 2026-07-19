45d72c72caa5623c6199005890a86923d577bfc6
Guilherme Macena <166922523+GUIDAX01@users.noreply.github.com>
2026-07-19 04:48:19 -0300

    inclusão de método put de usuário

--- src/main/java/com/guilherme/usuario/controller/UsuarioController.java
+++ src/main/java/com/guilherme/usuario/controller/UsuarioController.java
@@ -51,4 +51,10 @@ public class UsuarioController {
            return ResponseEntity.ok().build();
        }
 
+       @PutMapping
+       public ResponseEntity<UsuarioDTO> atualizandoDadosUsuario(@RequestBody UsuarioDTO dto,
+                                                                 @RequestHeader("Authorization") String token) {
+            return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
+       }
+
 }

9ef21ce1c4d47e188f3b12075507840b09d44d62
Guilherme Macena <166922523+GUIDAX01@users.noreply.github.com>
2026-08-05 04:37:49 -0300

    "Refatoração do método de login e autenticação de usuários"

--- src/main/java/com/guilherme/usuario/controller/UsuarioController.java
+++ src/main/java/com/guilherme/usuario/controller/UsuarioController.java
@@ -47,14 +47,8 @@ public class UsuarioController {
     @ApiResponse(responseCode = "200", description = "Usuário logado com sucesso")
     @ApiResponse(responseCode = "401", description = "Credencias inválidas")
     @ApiResponse(responseCode = "500", description = "Erro de servidor")
-    public String login (@RequestBody UsuarioDTO usuarioDTO){
-        Authentication authentication = authenticationManager.authenticate(
-
-                   new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
-                           usuarioDTO.getSenha())
-        );
-
-           return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    public ResponseEntity<String> login (@RequestBody UsuarioDTO usuarioDTO){
+        return ResponseEntity.ok(usuarioService.autenticarUsuario(usuarioDTO));
     }
 
     @GetMapping
---
+++ src/main/java/com/guilherme/usuario/infrastructure/exceptions/dto/ErrorResponseDTO.java
@@ -0,0 +1,18 @@
+package com.guilherme.usuario.infrastructure.exceptions.dto;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponseDTO {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String menssage;
+    private String path;
+}
